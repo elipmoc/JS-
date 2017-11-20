63cd743e0f74624dce531f582a24b4328559add8
elipmoc <relipmoc101@gmail.com>
2017-11-20 12:12:55 +0900

    初コミット

---
+++ lexer.js
@@ -0,0 +1,49 @@
+class Token {
+    constructor(tokenType, str) {
+        this._str = str;
+        this._tokenType = tokenType;
+    }
+
+    get str() { return this._str; }
+    get tokenType() { return this._tokenType;}
+}
+
+
+function test() {
+    var inputText = document.getElementById("inputText").value;
+    var numRe = /^[0-9]+/g;
+    var hoge=numRe.exec(inputText);
+    document.getElementById("resultText").value = hoge 
+    document.getElementById("resultText").value += "length:"+hoge[0].length;
+}
+
+function lexer() {
+    var inputText = document.getElementById("inputText").value;
+    var tokenList = new Array();
+    var numRe = /^[0-9]+/;
+    var opRe = /^[\+\-\*\/]/;
+    var temp;
+    while (true) {
+        temp = numRe.exec(inputText);
+        if (temp != null) {
+            inputText = inputText.substr(temp[0].length);
+            tokenList.push(new Token("num", temp[0]));
+            continue;
+        }
+        temp = opRe.exec(inputText);
+        if (temp != null) {
+            inputText = inputText.substr(temp[0].length);
+            tokenList.push(new Token("op", temp[0]));
+            continue;
+        }
+        break;
+    }
+    TokenListPrint(tokenList);
+}
+
+function TokenListPrint(tokenList) {
+    var resultText = document.getElementById("resultText");
+    tokenList.forEach(item=> {
+        resultText.value += item.tokenType+" : "+item.str+"\r\n";
+    });
+}
