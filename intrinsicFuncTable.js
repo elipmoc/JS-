ca95dc88c007f20099de90d42b2bcdf4d7a0d140
elipmoc <relipmoc101@gmail.com>
2017-12-06 10:47:53 +0900

    InなんとかFuncTable実装

--- intrinsicFuncTable.js
+++ intrinsicFuncTable.js
@@ -1,4 +1,21 @@
 //組み込み関数を登録しておくテーブル
 class IntrinsicFuncTable {
+    constructor() {
+        this._funcArray =
+            {
+                "max": { "body": (a) => { return a[0] > a[1] ? a[0] : a[1] }, "args": 2 }
+                , "min": { "body": (a) => { return a[0] > a[1] ? a[1] : a[0] }, "args": 2 }
+            }
+    }
 
+    //マッチする関数を返す
+    match(funcName, args) {
+        if (funcName in this._funcArray) {
+            let funcInfo = this._funcArray[funcName];
+            if (funcInfo["args"] == args) {
+                return funcInfo["body"];
+            }
+        }
+        return null;
+    }
 }
