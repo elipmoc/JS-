fa16b46680f2809aa4c385c1148862e509fed87a
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-12-15 17:05:30 +0900

    var_dump

--- index.php
+++ index.php
@@ -9,11 +9,13 @@ use Silex\Application;
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $app = new Application();
-/*$app->register(new Herrera\Pdo\PdoServiceProvider(),
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
                array(
                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
                    'pdo.username' => $dbopts["user"],
                    'pdo.password' => $dbopts["pass"]
                )
-);*/
+);
+
+var_dump($app['pdo']);
 echo file_get_contents("index.html");
