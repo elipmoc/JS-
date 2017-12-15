ca9af82e15f00883789068622aeca1260292ad89
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-12-15 14:44:46 +0900

    $appの正体を調べる

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+);
 
+var_dump($app);
 echo file_get_contents("index.html");
