718febeb324fa382ee626a8255f578555e3818b8
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-12-15 15:38:06 +0900

    10^10^10^10万回キレた猫

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ use Herrera\Pdo\PdoServiceProvider;
 use Silex\Application;
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
-$app = new Silex\Application();
+$app = new Application();
 /*$app->register(new Herrera\Pdo\PdoServiceProvider(),
                array(
                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
@@ -11,5 +11,4 @@ $app = new Silex\Application();
                    'pdo.password' => $dbopts["pass"]
                )
 );*/
-var_dump($app);
 echo file_get_contents("index.html");
