af6814555149a433e8d54345fa3eed87e0123374
elipmoc <relipmoc101@gmail.com>
2017-12-15 11:57:17 +0900

    operatorTable.jsを作成

---
+++ operatorTable.js
@@ -0,0 +1,18 @@
+﻿//組み込み演算子を定義しておくテーブル
+class OperatorTable {
+    constructor() {
+        this._opArray = {
+
+            "+": { "body": (a, b) => { return a + b; }, "priority":0,"infix":"left"}
+            , "-": { "body": (a, b) => { return a - b; }, "priority": 0, "infix": "left" }
+            , "*": { "body": (a, b) => { return a * b; }, "priority": 1, "infix": "left" }
+            , "/": { "body": (a, b) => { return a / b; }, "priority": 2, "infix": "left" }
+        }
+    }
+
+    getOpInfo(opName) {
+        if (opName in this._opArray)
+            return this._opArray[opName];
+        return null;
+    }
+}
