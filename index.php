6b0a757e7ce9115fb3e6f9265200c01b1618c8a0
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-12-15 15:22:35 +0900

    herokuキレそう

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+use Herrera\Pdo\PdoServiceProvider;
+use Silex\Application;
+
 $dbopts = parse_url(getenv('DATABASE_URL'));
+$app = new Application();
 $app->register(new Herrera\Pdo\PdoServiceProvider(),
                array(
                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
