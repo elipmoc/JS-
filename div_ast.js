7b7566fa3afa00410d3dad97d717c7bf1e841f6b
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-12-27 13:58:44 +0900

    Kindの実装

--- div_ast.js
+++ div_ast.js
@@ -1,14 +1,35 @@
 
+//何かの型を表すクラス
+hscalc.AnyType = class {
+    constructor(left, right) {
+    }
+
+    isAnyType() { return true; }
+}
+
 //型Kindを保持するクラス
 hscalc.Kind = class {
     constructor(left, right) {
         this._left = left;
         this._right = right;
     }
-    get Left() { return this._left; }
-    get Right() { return this._right; }
+    isAnyType() { return false; }
+    get left() { return this._left; }
+    get right() { return this._right; }
 }
 
+//Kindのleftを落とす
+hscalc.dropLeftKind = (kind) => {
+    if (kind.right.isAnyType()) {
+        return kind.right;
+    }
+    new hscalc.kind(kind.right.left, kind.right.right);
+}
+
+//KindまたはAnyTypeを結合して新たなKindを生成する
+hscalc.concatKindOrType = (a, b) => {
+    return new hscalc.kind(a, b);
+}
 
 //数値を表すAST 
 hscalc.ValueAst = class {
@@ -16,36 +37,36 @@ hscalc.ValueAst = class {
 
     constructor(value) {
         this._value = value;
-        this._kind = new Kind();
+        this._kind = new hscalc.AnyType();
     }
-    get Kind() { return this._kind; }
-    get Value() { return this._value; }
+    get kind() { return this._kind; }
+    get value() { return this._value; }
 }
 
 //関数名を表すAST
 hscalc.FuncNameAst = class {
-    get Kind() { }
+    get kind() { }
 }
 
 //関数に値を1つ適応するAST
 hscalc.FuncCallAst = class {
-    get Kind() { }
+    get kind() { }
 }
 
 //二項演算子のAST
 hscalc.BinaryAst = class {
-    get Kind() { }
+    get kind() { }
 
 }
 
 //リストを表すAST
 hscalc.ListAst = class {
-    get Kind() { }
+    get kind() { }
 
 }
 
 //数列表記（範囲指定）リストを表すAST
 hscalc.RangeListAst = class {
-    get Kind() { }
+    get kind() { }
 
 }
